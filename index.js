48cc2be0db039c26c6c31696edfe981ab1cc3746
Sharad <sharadritu.58.69@gmail.com>
2023-12-25 20:25:33 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 require('dotenv').config();
 const urlRoutes = require('./routes/url');
+const staticRoutes = require('./routes/staticRouter'); 
 const connectToMongoDB = require('./connection/connect');
 const PORT = 5000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
+
+app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 app.use('/url', urlRoutes);
+app.use('/', staticRoutes);
+
+app.get('/home', (req, res) => {
+    res.render('home');
+})
 
 const start = async () => {
     try {

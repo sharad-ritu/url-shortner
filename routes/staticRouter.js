f766264c25c382c634e65a225554b391fcb0af12
Sharad <sharadritu.58.69@gmail.com>
2023-12-26 19:31:04 +0545

    created router for register page

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const { getUrls } = require('../controllers/url');
 
 router.get('/', getUrls);
+router.get('/register', (req, res) => {
+    return res.render('register');
+});
 
 module.exports = router;

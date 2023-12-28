7118da0e2a4441cbd704d0c202bdcf4973931b3c
Sharad <sharadritu.58.69@gmail.com>
2023-12-28 20:43:34 +0545

    Update auth.js

--- services/auth.js
+++ services/auth.js
@@ -20,9 +20,24 @@ const jwt = require('jsonwebtoken');
 const secret = 'gandu-gandu-###';
 
 function setUser(user) {
-    return jwt.sign(user, secret);
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    }, secret);
 }
 
-function getUser() {
+function getUser(token) {
+    if (!token) return null;
 
+    try {
+        return jwt.verify(token, secret);
+    } catch (error) {
+        return null;
+    }
+    
+}
+
+module.exports = {
+    setUser,
+    getUser,
 }
